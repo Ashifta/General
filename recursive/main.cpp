c0aa7f866f7cfd3d02b76159fc81f71677cefbd8
ashif <ashiftaec@gmail.com>
2017-10-02 01:13:29 +0530

    Updated with more comments...

--- recursive/main.cpp
+++ recursive/main.cpp
@@ -1,29 +1,59 @@
 #include <QCoreApplication>
 #include <QtDebug>
 
-///---------------------There is an almghty, he sets maximumDays for our life and condition as well:)----------
-int INITIALCOUNT = 1;
+
+///I think philosophy of recussive function maps to the life and death?
+
+
+
+///---------------There is an almghty, he sets maximumDays for our life and condition as well:)----------
+
+
+////Angel is Thread, who monitor and do the the process
+/// God is Programmer, the domain expert, the product owner:) and many...
+int angelCountGoodDeeds = 0;
+int angelCountBadDeeds = 0;
 int MAXIMUMDAYS = 5;//In global space Almightly set the maximum number of days he/she to live.
-bool liveToday()
+
+bool liveToday(int myDay)
 {
-    qDebug()<< INITIALCOUNT;
-    INITIALCOUNT++;
-    if(INITIALCOUNT == MAXIMUMDAYS )///Time of death comes here, reason is well defined statically/dynamically return false.
-    {
+    if(myDay == MAXIMUMDAYS )///Time of death comes here, reason is well defined statically/dynamically condition met
+    {   qDebug()<< "Today is my last day to live, Can I have few more day to live?!";
+        qDebug()<< "Angel said, there is no more chance you need to return now.., since it is your time to unwind.!!";
+        qDebug()<< "Angel said, Dont worry, your have alreay marked your good/bad days, you will get what you deserve, After all it is I who is the supreme owner of judgement:)";
         return false; //Dead "Now Stack unwind, you are resposiblet to what you have done in each day:)
     }
     else
     {
-        return liveToday(); //start another day, make sure there is a return at anytime.
+        if(myDay== 2 || myDay==3 || myDay==4 )//Why I have put condition is effort is required for good deed, its not simple.
+        {
+           angelCountGoodDeeds++;
+           qDebug()<< "I have good deeds Record:) for this day: "<< myDay;
+        }
+        else
+        {
+           angelCountBadDeeds++;
+           qDebug()<<"I have Bad deed Record:( for this day: "<< myDay;
+        }
+        return liveToday(++myDay); //start another day
     }
 }
 
-///
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication a(argc, argv);//universal environment
 
     //This is main thread of life
-    liveToday();//Initated by Almighty
+    liveToday(1);//Initated by Almighty, from day 1.
+
+    ///Judgement day...
+    if( angelCountGoodDeeds >= angelCountBadDeeds )
+    {
+        qDebug() << "WINNER";
+    }
+    else
+    {
+        qDebug() << "LOOSER";
+    }
     return a.exec();
 }
